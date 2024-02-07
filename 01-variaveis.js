e1e81a025787268954d61062df522a9da3cce618
Thiago Vanucci <vanucci.thiagocandarola@gmail.com>
2024-02-07 11:26:12 -0300

    JavaScript - basico

---
+++ 01-variaveis.js
@@ -0,0 +1,12 @@
+//Criando variáveis com "var" (var nome_variavel = <valor>;)
+//var cria variavel global 
+
+var nome = "Thiago";
+var idade = 16;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+console.log("agora fiz aniversário! 🥳 Tenho", idade + 1, "anos!");
+
+//aumentando automagicamente +1 (autoincremento)
+idade++ //idade = idade + 1
+console.log("agora fiz aniversário! 🥳 Tenho", idade, "anos!")
---
+++ 02-aritmetico.js
@@ -0,0 +1,28 @@
+//utilizando operadores aritmeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois numeros informados por voce (var nome_variavel = 12)
+
+var x = 4;
+var y = 8;
+
+console.log("A soma entre y e x é:", y + x);
+
+var soma = x + y;
+var subtração = x - y;
+var multiplicação = x * y;
+var divisão = x / y;
+console.log(""); //pular linha
+
+console.log("soma =", soma);
+console.log("subtração = ", subtração);
+console.log("multiplicação = ", multiplicação);
+console.log("divisão = ", divisão);
+
+//regras para nomear identificadores (var, const ou lest) podem começar com A-Z, a,z, _ ou $
+
+var MinhaVariavel = 10;
+var outravez = 20;
+var _minhaconstante = 3.1514;
+var $minhavariavel = 11;
